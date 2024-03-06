7ce7a6f150f84a1f4685df63a80cae8faf1eeb38
Колотев Сергей <Sergey.Kolotev@glowbyteconsulting.com>
2024-03-06 20:45:12 +0300

    Mongodb

--- src/main/java/tacos/data/repos/OrderRepository.java
+++ src/main/java/tacos/data/repos/OrderRepository.java
@@ -3,7 +3,6 @@ package tacos.data.repos;
 import org.springframework.data.repository.CrudRepository;
 import tacos.data.TacoOrder;
 
-import java.util.UUID;
 
-public interface OrderRepository extends CrudRepository<TacoOrder, UUID> {
+public interface OrderRepository extends CrudRepository<TacoOrder, String> {
 }
